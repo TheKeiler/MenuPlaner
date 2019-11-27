632bf45253d71f90645a6b1617f977a20c56e967
Patrick Keist <p.keist@hotmail.com>
2019-11-27 14:00:51 +0100

    Initial Android App

---
+++ ContinuousIntegration/build.fsx
@@ -0,0 +1,23 @@
+#r @packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+open BuildHelpers
+
+Target "common-build" (fun () ->
+	RestorePackages "MenuePlanerPCL.sln"
+	
+	MSBuild "MenuePlaner/bin/Debug" "Build" [ (
+	"Configuration", "Debug"); ("Plattform", "Any CPU"
+	) ] [ "MenuePlanerPCL.sln" ] |> ignore
+)
+
+Target "common-tests" (fun () ->
+	RunNUnitTests "Todo/bin/Debug/MenueplanerPCL.Tests.dll"
+	"MenuePlaner/bin/Debug/testresults.xml" |> ignore
+)
+
+"common-build"
+ ==> "common-tests"
+ 
+RunTargetOrDefault "common-tests"
+ 
