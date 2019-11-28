90ae0c16e6e356b9501089643655408288ebb02f
Patrick Keist <p.keist@hotmail.com>
2019-11-28 08:16:48 +0100

    CI

--- build.bat
+++ build.bat
@@ -2,5 +2,3 @@ echo version %1
 
 "tools\NuGet\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
 "tools\Fake\tools\Fake.exe" "build.fsx" version=%1
-
-let version = getBuildParam "version"
--- build.fsx
+++ build.fsx
@@ -1,23 +1,56 @@
-#r @packages/FAKE/tools/FakeLib.dll"
+#r "paket:
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.MSBuild
+nuget Fake.DotNet.Testing.NUnit
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
 
-open Fake
-open BuildHelpers
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.DotNet
+open Fake.DotNet.Testing
+open Fake.Core
 
-Target "common-build" (fun () ->
-	RestorePackages "MenuePlaner.sln"
-	
-	MSBuild "MenuePlaner/bin/Debug" "Build" [ (
-	"Configuration", "Debug"); ("Plattform", "Any CPU"
-	) ] [ "MenuePlaner.sln" ] |> ignore
+// Properties
+let buildDir = "./build/"
+let testDir  = "./test/"
+
+// Targets
+Target.create "Clean" (fun _ ->
+    Shell.CleanDirs [buildDir; testDir]
 )
 
-Target "common-tests" (fun () ->
-	RunNUnitTests "MenuePlaner/bin/Debug/Menueplaner.Tests.dll"
-	"MenuePlaner/bin/Debug/testresults.xml" |> ignore
+Target.create "common-build" (fun _ ->
+   !! "src/app/**/*.csproj"
+     |> MSBuild.runRelease id buildDir "common-build"
+     |> Trace.logItems "AppBuild-Output: "
 )
 
-"common-build"
- ==> "common-tests"
+Target.create "common-tests" (fun _ ->
+    !! "src/test/**/*.csproj"
+      |> MSBuild.runDebug id testDir "Build"
+      |> Trace.logItems "TestBuild-Output: "
+)
+
+Target.create "Test" (fun _ ->
+    !! (testDir + "/NUnit.Test.*.dll")
+      |> NUnit3.run (fun p ->
+          {p with
+                ShadowCopy = false })
+)
+
+Target.create "Default" (fun _ ->
+    Trace.trace "Hello World from FAKE"
+)
+
  
+// Dependencies
+open Fake.Core.TargetOperators
+"Clean"
+  ==> "common-build"
+  ==> "common-tests"
+  ==> "Default"
+
+// start build
 RunTargetOrDefault "common-tests"
  
