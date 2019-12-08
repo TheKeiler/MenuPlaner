90a3fb1ccf43e6bd71e1e7802633bbaad0ab8155
Patrick Keist <p.keist@hotmail.com>
2019-12-08 12:11:32 +0100

    Commited new Test-Library

---
+++ MenuPlanerApp/MenuPlanerApp.Core.Tests/Model/IngredientsTest.cs
@@ -0,0 +1,48 @@
+﻿using MenuPlanerApp.Core.Model;
+using NUnit.Framework;
+
+
+namespace MenuPlanerApp.Core.Tests.Model
+{
+    [TestFixture]
+    internal class IngredientsTest
+    {
+        [Test]
+        public void EqualsWithTwoSameObjectsShouldBeTrue()
+        {
+            //Arrange
+            var ingredient1 = new Ingredient();
+            var ingredient2 = new Ingredient();
+
+            ingredient1.Name = "Test";
+            ingredient1.Description = "frisch";
+
+            ingredient2.Name = "Test";
+            ingredient2.Description = "frisch";
+
+            //Act
+            var equal = ingredient1.Equals(ingredient2);
+            //Assert
+            Assert.That(equal, Is.True);
+        }
+
+        [Test]
+        public void EqualsWithTwoDiffrentObjectsShouldBeFalse()
+        {
+            //Arrange
+            var ingredient1 = new Ingredient();
+            var ingredient2 = new Ingredient();
+
+            ingredient1.Name = "Test";
+            ingredient1.Description = "frisch";
+
+            ingredient2.Name = "Test2";
+            ingredient2.Description = "frisch";
+
+            //Act
+            var equal = ingredient1.Equals(ingredient2);
+            //Assert
+            Assert.That(equal, Is.False);
+        }
+    }
+}
