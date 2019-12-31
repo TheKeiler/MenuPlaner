00be74b6aa45e223e2eac3aff3512f9ff9fce58e
Patrick Keist <p.keist@hotmail.com>
2019-12-31 16:45:32 +0100

    CodeCleanUp

---
+++ MenuPlanerApp/MenuPlanerApp.API.Tests/Controllers/IngredientsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace MenuPlanerApp.API.Tests.Controllers
+{
+    [TestFixture]
+    internal class IngredientsControllerTest
+    {
+        [Test]
+        public void GetReturnsIngredients()
+        {
+            /*
+            // Arrange
+            var mockOptions = new Mock<DbContextOptions>();
+            var mockContext = new Mock<MenuPlanerAppApiContext>(mockOptions).Object;
+                var controller = new IngredientsController(mockContext);
+
+            // Act
+            var response = controller.GetIngredient(2);
+
+            // Assert
+            Assert.That(response.Result.Value.Id.Equals(2));
+            */
+        }
+    }
+}
